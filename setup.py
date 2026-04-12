e101b3a3f3d26f2ef2d311bc99dc0809e91414b6
crazykuma <crazykuma@qq.com>
2026-04-12 16:37:51 +0800

    chore: add pyproject.toml for PyPI packaging

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="fns-cli",
@@ -8,6 +8,8 @@ setup(
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/crazykuma/fns-cli",
+    license="MIT",
+    license_files=["LICENSE"],
     py_modules=["fns"],
     python_requires=">=3.6",
     install_requires=[
